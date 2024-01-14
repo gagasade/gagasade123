08b86452ee50a7d0a7cb0fc84d987e7702d781cb
gagasade <156615844+gagasade@users.noreply.github.com>
2024-01-15 05:16:20 +0700

    Add files via upload

---
+++ tentang-kami.php
@@ -0,0 +1,114 @@
+<?php
+    require "koneksi.php";
+    $queryProduk = mysqli_query($con, "SELECT id, nama, harga, foto, detail FROM produk LIMIT 12");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Musik Degas | Home</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "navbar.php"; ?>
+
+    <div class="container-fluid banner d-flex align-items-center">
+        <div class="container text-center text-white">
+            <h1>Music Shop Degas</h1>
+            <h3>Mau Cari Apa?</h3>
+            <div class="col-md-8 offset-md-2">
+                <form method="get" action="produk.php">
+                    <div class="input-group input-group-lg my-4">
+                        <input type="text" class="form-control" placeholder="Nama Barang" 
+                        aria-label="Recipient's username" aria-describedby="basic-addon2" name="keyword">
+                        <button type="submit" class="btn warna2 text-white">Search</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!--highlighted kategori-->
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3>Kategori Terlaris</h3>
+
+            <div class="row mt-5x">
+                <div class="col-md-3 mb-2">
+                    <div class="highlighted-kategori kategori-guitar d-flex justify-content-center
+                    align-items-center">
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=guitar">Gitar & Bass</a></h4>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="highlighted-kategori kategori-drum d-flex justify-content-center
+                    align-items-center">
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=drum">Drum & Perkusi</a></h4>">
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="highlighted-kategori kategori-piano d-flex justify-content-center
+                    align-items-center">
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=piano">Keyboard & Piano</a></h4>">
+                    </div>
+                </div>
+                <div class="col-md-3 mb-2">
+                    <div class="highlighted-kategori kategori-mic d-flex justify-content-center
+                    align-items-center">
+                        <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=mic">Vocal & Microphone</a></h4>">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- tentang kami-->
+    <div class="container-fluid warna3 py-5">
+        <div class="container text-center">
+            <h3>Tentang Kami</h3>
+            <p class="fs-5 mt-3">
+            Hanya menyediakan produk berkualitas tinggi, kami memastikan setiap alat 
+            musik di toko kami memenuhi standar mutu yang ketat.
+            Mari bersama-sama menciptakan pengalaman musik yang tak terlupakan 
+            dan membagikan kebahagiaan melalui alunan nada indah.
+            </p>
+        </div>
+    </div>
+
+    <!--produk-->
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3>Produk</h3>
+
+            <div class="row mt-5">
+                <?php while($data = mysqli_fetch_array($queryProduk)){ ?>
+                <div class="col-sm-6 col-md-4 mb-3">
+                    <div class="card h-100">
+                        <div class="image-box">
+                            <img src="image/<?php echo $data['foto']; ?>" class="card-img-top" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title"><?php echo $data['nama']; ?></h4>
+                            <p class="card-text text-truncate"><?php echo $data['detail']; ?></p>
+                            <p class="card-text text-harga">Rp <?php echo $data['harga']; ?></p>
+                            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn 
+                            warna3 text-white">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+            <a class="btn btn-outline-warning mt-3 p-2 fs-3" href="produk.php">See 
+                More</a>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+</html>
